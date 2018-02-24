5d6a40b57028404ee8310200de5a2557438fe32e
Skysplit <manetamajster@gmail.com>
2018-02-24 14:27:11 +0100

    Use webpack-node-externals instead of own function

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,7 @@
 const { CheckerPlugin, TsConfigPathsPlugin } = require('awesome-typescript-loader')
+const nodeExternals = require('webpack-node-externals');
 const path = require('path');
 const fs = require('fs');
-
-const nodeModules = fs.readdirSync('node_modules')
-  .filter((mod) => !['.bin'].includes(mod))
-  .reduce((modules, mod) => Object.assign(modules, {
-    [mod]: `commonjs ${mod}`
-  }))
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/server.ts'),
@@ -16,7 +11,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'commonjs',
   },
-  externals: nodeModules,
+  externals: nodeExternals({
+    modulesFromFile: true,
+  }),
   resolve: {
     extensions: ['.ts', '.js'],
     plugins: [
